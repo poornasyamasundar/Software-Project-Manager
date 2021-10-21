51ab5acd763b03cef869229d5a5340da0c16a802
GannaYashwanth <cs19b016@iittp.ac.in>
2021-10-21 23:49:03 +0530

    more ajax

--- SoftwareManager/homepage/views.py
+++ SoftwareManager/homepage/views.py
@@ -6,8 +6,9 @@ from Databases.create import isUser, isTrueCredentials, insertUser
 from Databases.creation import getDetails, insertDetails, updateDetails
 from Databases.meetings_table import createTable, specificMeetings, allMeetings, deleteMeetings, modifyMeeting, insertMeeting
 from Databases.tasks_table import create_taskTable, specificTasks, allTasks, deleteTask, modifyTask, insertTask, getTasks
-from Databases.feedback import insertFeedback, deleteFeedback, modifyFeedback, allFeedback
+from Databases.feedback import insertFeedback, deleteFeedback, modifyFeedback, allFeedback, getComments
 from Databases.projects_table import create_projecttable, insertProject, modifyProject, deleteProject, allProjects, specificProjects
+from Databases.notices_table import create_noticeTable, insertNotice, modifyNotice, deleteNotice, specificNotices, getNotices
 import json
 from django.views.decorators.csrf import csrf_exempt
 import datetime
@@ -16,7 +17,6 @@ def index(request):
     print("**********************************")
     print("Printed to terminal")
     #use the functions in databases to get the data
-
 
     print("**********************************")
     return render(request, 'index.html')
@@ -39,6 +39,27 @@ def to_getTasksPy(request):
         number = request.POST['number']
         type = request.POST['type_']
         result = getTasks(table_name, number , type)
+        #print(result)
+        return HttpResponse(json.dumps(result))
+
+@csrf_exempt
+def to_getNoticesPy(request):
+    #print(request.method)
+    #print(request.POST)
+    if request.method == 'POST':
+        table_name = request.POST['table_name']
+        number = request.POST['number']
+        result = getNotices(table_name, number)
+        #print(result)
+        return HttpResponse(json.dumps(result))
+
+@csrf_exempt
+def to_getCommentsPy(request):
+    #print(request.method)
+    #print(request.POST)
+    if request.method == 'POST':
+        number = request.POST['number']
+        result = getComments(number)
         #print(result)
         return HttpResponse(json.dumps(result))
 
@@ -92,6 +113,29 @@ def taskFunctionPy(request):
 
         return HttpResponse("y")
 
+
+@csrf_exempt
+def noticeFunctionPy(request):
+    if request.method == 'POST':
+
+        table_name = request.POST['table_name']
+        type = request.POST['type_']
+        id = request.POST['id']
+        createdBy = request.POST['createdBy']
+        createdOn = request.POST['createdOn']
+        description = request.POST['description']
+
+        date = str(datetime.datetime.today())
+        date = date[0:10]
+
+        if type == '1':
+            insertNotice(table_name, createdBy, date, description)
+        elif type == '2':
+            modifyNotice(table_name, id, createdBy, createdOn, description)
+        elif type == '3':
+            deleteNotice(table_name, id)
+
+        return HttpResponse("n")
 
 #sample function to process a ajax GET request
 def testGET(request):
